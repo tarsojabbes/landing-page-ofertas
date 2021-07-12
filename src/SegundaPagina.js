2de9e9438a9f20df5cfcde81adfd0c94aa5b8363
Tarso Jabbes <tarsojabbes@gmail.com>
2021-07-12 17:42:56 -0300

    Estilização dos cards

--- src/SegundaPagina.js
+++ src/SegundaPagina.js
@@ -14,24 +14,39 @@ export default function SegundaPagina() {
                             <div className="row">
                                 <div className="col col-card">
                                     <div className="product-card">
-
+                                        <div className="img-center">
+                                            <img src="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-xr-red-select-201809?wid=940&hei=1112&fmt=p-jpg&qlt=95&.v=1551226038669"
+                                                alt="iPhone XR" />
+                                        </div>
+                                        <p>iPhone XR 64GB</p>
+                                        <p><s>R$4.999.00</s></p>
+                                        <h4>R$4.499.99</h4>
                                     </div>
                                 </div>
                                 <div className="col col-card">
                                     <div className="product-card">
-
+                                        <img src="https://cdn.awsli.com.br/600x450/991/991246/produto/36649270/0fda51e913.jpg" alt="iPhone XS Max" />
+                                        <p>iPhone XS Max 64GB</p>
+                                        <p><s>R$3.899.99</s></p>
+                                        <h4>R$3.599,99</h4>
                                     </div>
                                 </div>
                             </div>
                             <div className="row" style={{ paddingTop: 30 }}>
                                 <div className="col col-card">
                                     <div className="product-card">
-
+                                        <img id="mac" src="https://images-na.ssl-images-amazon.com/images/I/31sq02%2BO6HL._AC_.jpg" alt="Macbook Pro" />
+                                        <p>Macbook Pro 13" 256GB</p>
+                                        <p><s>R$9.199.99</s></p>
+                                        <h4>R$8.899.99</h4>
                                     </div>
                                 </div>
                                 <div className="col col-card">
                                     <div className="product-card">
-
+                                        <img src="https://images-na.ssl-images-amazon.com/images/I/51IYu924UoL._AC_SL1000_.jpg" alt="iPad Pro" />
+                                        <p>iPad Pro 10.9" 256GB</p>
+                                        <p><s>R$6.099.00</s></p>
+                                        <h4>R$5.899.99</h4>
                                     </div>
                                 </div>
                             </div>
