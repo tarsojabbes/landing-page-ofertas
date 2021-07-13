2ac44d0963dc648bc9475f89ee2aba51615a1403
Tarso Jabbes <tarsojabbes@gmail.com>
2021-07-13 17:07:42 -0300

    Adição do React

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import SegundaPagina from './SegundaPagina'
 import Footer from './Footer'
 
 export default function App() {
+
+  const [nome, setNome] = useState('')
+  const [email, setEmail] = useState('')
+
+  const cadastrar = (n, e) => {
+    localStorage.setItem('nome', n)
+    localStorage.setItem('email', e)
+    alert(`Obrigado, ${localStorage.getItem('nome')}! Seu email ${localStorage.getItem('email')} foi cadastrado`)
+  }
+
   return (
     <>
       <div className="container main">
@@ -13,15 +23,20 @@ export default function App() {
           </div>
           <div className="col d-flex-col">
             <div className="card">
+
               <label className="text-color-white">SEU NOME COMPLETO</label>
-              <input type="text" className="input-text" required />
+              <input type="text" className="input-text" required value={nome} onChange={(e) => setNome(e.target.value)} />
+
               <label className="text-color-white">SEU EMAIL</label>
-              <input type="email" className="input-text" required />
+              <input type="email" className="input-text" required value={email} onChange={(e) => setEmail(e.target.value)} />
+
               <div className="form-check form-switch">
                 <input className="form-check-input" type="checkbox" id="flexSwitchCheckDefault" />
                 <label className="form-check-label text-color-white" for="flexSwitchCheckDefault">Eu aceito os <a href="/"> Termos e Condições de Serviço</a></label>
               </div>
-              <button>CADASTRAR</button>
+
+              <button type="button" onClick={() => cadastrar(nome, email)}>CADASTRAR</button>
+
             </div>
           </div>
         </div>
